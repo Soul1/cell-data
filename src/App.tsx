4e44b850a72152ed0c9669ca6c987b2e1abad7c9
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-08-07 18:53:25 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {h, FunctionalComponent } from 'preact'
+
+type TProps = {}
+
+const App: FunctionalComponent<TProps> = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { h, render } from 'preact'
+import App from './App'
+
+import './styles/index.scss'
+
+render(<App />, document.getElementById('root'))
