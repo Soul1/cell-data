04f15056ced0145d5b85e9adbbb6cdb0c6cd00db
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-08-09 00:42:20 +0300

    add store through ContextApi, used Provider component of unisote/preact

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
-import { h, render } from 'preact'
+import {h, render} from 'preact'
 import App from './App'
-
+import {Provider} from 'unistore/preact'
 import './styles/index.scss'
 
-render(<App />, document.getElementById('root'))
+import store from './store'
+
+render(<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'))
