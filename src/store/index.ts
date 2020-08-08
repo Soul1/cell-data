14c44998e805e6deb613bc8dbb2ddd63484ea5f5
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-08-09 00:43:14 +0300

    create store and action setDataArr for table

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import createStore from 'unistore'
+
+export const actions = {
+  setDataArr: async () => {
+    return {dataArr: await fetch('/data.json').then(r => r.json()).then(r => r)}
+  }
+}
+
+const store = createStore({dataArr: null})
+
+export default store
+
