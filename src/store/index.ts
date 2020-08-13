a47d50ac08db4c2bd2b8b72df390db9aed2f3897
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-08-13 14:20:19 +0300

    Made type for dataArr and add new action setCell for additional table

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,26 @@
 import createStore from 'unistore'
 
+export type TData = {
+  svcId: number,
+  ctime: string,
+  svcType: string,
+  userName: string,
+  feedback: number,
+  comment: string
+}
+
+export type TDataArr = TData[]
+
 export const actions = {
   setDataArr: async () => {
     return {dataArr: await fetch('/data.json').then(r => r.json())}
+  },
+  setCell: (cell: []) => {
+    return {dataArr: cell}
   }
 }
 
-const store = createStore({dataArr: null})
+const store = createStore({dataArr: null, cell: []})
 
 export default store
 
