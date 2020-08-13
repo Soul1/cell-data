7ff0ddedfec1f2efaf98e5fa1e8089a7b8658bed
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-08-13 14:27:07 +0300

    Imported type TDataArr and add new action setCell and passed as props to the GridTable

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import {h, FunctionalComponent} from 'preact'
 import {useEffect} from 'preact/hooks'
 import {connect} from 'unistore/preact'
 import GridTable from './components/GridTable'
-import {actions} from './store'
+import {actions, TDataArr} from './store'
 
 type TProps = {
   setDataArr: () => void
-  dataArr: []
+  setCell: () => void
+  dataArr: TDataArr
 }
 
-const App: FunctionalComponent<TProps> = ({setDataArr, dataArr}) => {
-  
+const App: FunctionalComponent<TProps> = ({setCell, setDataArr, dataArr}) => {
+
   useEffect(() => setDataArr(), [])
 
-  return dataArr ? <GridTable dataArr={dataArr}/> : 'Initializing'
+  return dataArr ? <GridTable dataArr={dataArr} setCell={setCell}/> : 'Initializing'
 }
 
 export default connect('dataArr', actions)(App)
