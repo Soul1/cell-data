872fac435cbdedada0968b1fa1c744de4da8ad3f
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-08-16 00:48:08 +0300

    added new component AdditionalTable,update markup, added display AdditionalTable and connected of state rows

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,29 @@ import {h, FunctionalComponent} from 'preact'
 import {useEffect} from 'preact/hooks'
 import {connect} from 'unistore/preact'
 import GridTable from './components/GridTable'
-import {actions, TDataArr} from './store'
+import {actions, TDataArr, TRows} from './store'
+import AdditionalTable from './components/AdditionalTable'
 
 type TProps = {
   setDataArr: () => void
-  setCell: () => void
+  setRows: (row: TRows) => void
   dataArr: TDataArr
+  rows: TRows
 }
 
-const App: FunctionalComponent<TProps> = ({setCell, setDataArr, dataArr}) => {
+const App: FunctionalComponent<TProps> = ({setRows, setDataArr, dataArr, rows}) => {
 
   useEffect(() => setDataArr(), [])
-
-  return dataArr ? <GridTable dataArr={dataArr} setCell={setCell}/> : 'Initializing'
+  return (
+    <div class='app'>
+      <div className='main-table'>
+        {!!dataArr ? <GridTable dataArr={dataArr} setRows={setRows}/> : 'Initializing'}
+      </div>
+      <div className='additional-table'>
+        {!!rows && <AdditionalTable rows={rows}/>}
+      </div>
+    </div>
+  )
 }
 
-export default connect('dataArr', actions)(App)
+export default connect(['dataArr', 'rows'], actions)(App)
