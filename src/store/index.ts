01c9fd424de38c725dacbc5c32e7331d060ffcef
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-08-18 04:14:46 +0300

    remove of state filterDateDataArr and acton setFilterDateDataArr

--- src/store/index.ts
+++ src/store/index.ts
@@ -25,14 +25,10 @@ export const actions = {
   },
   setRows: (state: any, rows: TRows) => {
     return {rows}
-  },
-  setFilterDateDataArr: (state: any, filterDateDataArr: TDataArr) => {
-    console.log(filterDateDataArr, 'yeh')
-    return {filterDateDataArr}
   }
 }
 
-const store = createStore({dataArr: null, rows: null, filterDateDataArr: null})
+const store = createStore({dataArr: null, rows: null})
 
 export default store
 
