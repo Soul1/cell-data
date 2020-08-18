0c38e466282f28531fe82356eb21c246036e6e77
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-08-18 04:38:26 +0300

    import type DateInterval, added onChange to transfer intervals to and after selected dates in App component

--- src/components/FilterDate/index.tsx
+++ src/components/FilterDate/index.tsx
@@ -1,24 +1,31 @@
 import {h, FunctionalComponent} from 'preact'
-import {StateUpdater} from 'preact/hooks'
+import {useEffect, useState} from 'preact/hooks'
+import {DateInterval} from '../../App'
 
 import './index.scss'
 
 type TProps = {
-  setDateTo: StateUpdater<Date>
-  setDateAfter: StateUpdater<Date>
+  onChange: (interval: DateInterval) => void
 }
 
-const FilterDate: FunctionalComponent<TProps> = ({setDateTo, setDateAfter}) => {
+const FilterDate: FunctionalComponent<TProps> = ({onChange}) => {
+  const [dateTo, onDateTo] = useState<null | Date>(null)
+  const [dateAfter, onDateAfter] = useState<null | Date>(null)
+
+  useEffect(() =>
+      onChange({to: dateTo, after: dateAfter}),
+    [dateTo, dateAfter]
+  )
 
   return (
     <div class='filter-date'>
       <div className="filter-date__to">
         <b>От</b>
-        <input type="date" onChange={e => setDateTo(new Date(e.target.valueAsDate))}/>
+        <input type="date" onChange={e => onDateTo(new Date(e.target.valueAsDate))}/>
       </div>
       <div className="filter-date__after">
         <b>До</b>
-        <input type="date" onChange={e => setDateAfter(new Date(e.target.valueAsDate))}/>
+        <input type="date" onChange={e => onDateAfter(new Date(e.target.valueAsDate))}/>
       </div>
     </div>
   )
